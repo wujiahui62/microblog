04729bc2083831c01074eaaa6ad344af67fcdf4a
Jiahui Wu <wujiahui62@gmail.com>
2018-05-10 12:56:15 -0400

    changed report

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-source venv/bin/activate
+# source venv/bin/activate
 export FLASK_APP=microblog.py
 export FLASK_DEBUG=0
 export MAIL_SERVER=smtp.googlemail.com
@@ -7,5 +7,5 @@ export MAIL_USE_TLS=1
 export MAIL_USERNAME=libaixiu2014@gmail.com
 export MAIL_PASSWORD=cloud_app
 export MS_TRANSLATOR_KEY=5279b9fe290f4c84a7a4d15b926a4a45
-flask run
+# flask run
 
